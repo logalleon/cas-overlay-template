aec1f8ad85a19a203d78f79bd361126fdc5a5c23
Richard Persche <rpersche@us.hyperwave.com>
2017-07-25 14:13:09 -0400

    Hide body until shown in iFrame. Trigger loading of iFrame

--- src/main/resources/static/js/eclkc.js
+++ src/main/resources/static/js/eclkc.js
@@ -1,1 +1,5 @@
-console.log('Hello ECLKC');
+if (top.frames.length === 0){
+  window.location.replace('https://eclkcdev.cleverex.com/user-management?url=' + encodeURIComponent(window.location.href) );
+} else {
+  jQuery("body").show();
+}
