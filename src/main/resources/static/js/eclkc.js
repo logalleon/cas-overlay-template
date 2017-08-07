7c7c2565f18b21955651838a7b9514506e26c049
Richard Persche <rpersche@us.hyperwave.com>
2017-08-07 14:24:35 -0400

    JavaScript cleanup and lazy registration

--- src/main/resources/static/js/eclkc.js
+++ src/main/resources/static/js/eclkc.js
@@ -1,28 +1,64 @@
-G_SERVER = '';
+/* global $ */
+var G_SERVER = '';
 // Fires before ready (since it's a redirect)
-if (top.frames.length === 0) {
+if (top.frames.length === 0 && $('.alert-danger').length === 0) {
   window.location.replace(G_SERVER + '/user-management?url=' + encodeURIComponent(window.location.href));
 } else {
-  jQuery("body").show();
+  $('body').show();
 }
+
 
 // Ready
 $(function() {
-  var $form = $('#fm1');
   var $submit = $('.btn-submit[type="submit"]');
   $submit.closest('.row').prepend(
-    '<div id="message" class="alert alert-error"><p></p></div>'
+    '<div id="message" class="alert alert-success"><p></p></div>'
   );
-  var $message = $('#message');
-  $message.css({
+  $submit.closest('.row').prepend(
+    '<div id="errormessage" class="alert alert-error"><p></p></div>'
+  );
+  var $errormessage = $('#errormessage');
+  $errormessage.css({
     'background-color': '#f2dede',
     'color': '#a94442',
     'margin': '.5rem 0'
   }).hide();
+  var $message = $('#message');
+  $message.css({
+    'background-color': '#dff0d8',
+    'border-color': '#d6e9c6',
+    'color': '#3c763d',
+    'margin': '.5rem 0'
+  }).hide();
   var credentialsWarning = 'Username or password is invalid.';
+
+  //load lazy registration
+  $('body').append('<div class="lazy-registration-container" id="lazy-registration-container"></div>');
+  $('#lazy-registration-container').load('/users/api/static/html/lazyregistration.html', function (){
+
+    $('#register').on('click', function(e) {
+      e.preventDefault();
+      $('#lazyRegistration').show();
+      $(window).scrollTop($('#lazyRegistration button[type="submit"]').offset().top);
+    });
+
+    $('#lazyRegForm').on('submit', function(e) {
+      validateLazyRegForm(e);
+    });
+
+    $('#lazyRegUsername').on('blur', function (e){
+      checkUsernameAvailability();
+    });
+
+    resizeFrame(true);
+
+  });
+
   $submit.on('click', function(e, tokenValidated) {
     if (!tokenValidated) {
       e.preventDefault();
+      $errormessage.hide();
+      $message.hide();
       var data = {
         uid: $('#username').val(),
         password: $('#password').val(),
@@ -35,29 +71,34 @@ $(function() {
         contentType: 'application/json',
         crossOrigin: true,
         success: function(response) {
-          console.log(response);
           if (response.error) {
-            $message.show().find('p').html(response.error.message);
+            $errormessage.show().find('p').html(response.error.message);
+            resizeFrame();
           } else {
-            //$(e.currentTarget).trigger('click', true);
+            //send token with password field
+            $('#password').val($('#password').val() + '<token>' + $('#token').val());
+            $(e.currentTarget).trigger('click', true);
           }
-        }.bind(this),
+        },
         error: function(jqxhr, status, err) {
           if (jqxhr.status === 401) {
-            $message.show().find('p').text(credentialsWarning)
+            $errormessage.show().find('p').text(credentialsWarning);
           }
-        }.bind(this),
+          resizeFrame();
+        },
         complete: function() {
 
-        }.bind(this)
+        }
       });
     }
   });
 
-  let $email2falink = $('#email2falink')
+  let $email2falink = $('#email2falink');
   $email2falink.on('click', function(e) {
     e.preventDefault();
-    var data = {
+    $errormessage.hide();
+    $message.hide();
+    let data = {
       uid: $('#username').val(),
       password: $('#password').val(),
       token: $('#text').val()
@@ -69,29 +110,33 @@ $(function() {
       contentType: 'application/json',
       crossOrigin: true,
       success: function(response) {
-        console.log(response);
         if (response.error) {
-          $message.show().find('p').html(response.error.message);
+          $errormessage.show().find('p').html(response.error.message);
         } else {
-          //$(e.currentTarget).trigger('click', true);
+          $message.show().find('p').html(response.message);
         }
-      }.bind(this),
+        resizeFrame();
+      },
       error: function(jqxhr, status, err) {
         if (jqxhr.status === 401) {
-          $message.show().find('p').text(credentialsWarning)
+          $errormessage.show().find('p').text(credentialsWarning);
         }
-      }.bind(this),
+        resizeFrame();
+      },
       complete: function() {
 
-      }.bind(this)
+      }
     });
 
   });
+
+
 });
 
-
-function enroll2fa() {
-  let $form = $('#fm1');
+/**
+ * Function that will be called from return message if use is not enrolled
+ */
+function enroll2fa() {  //eslint-disable-line
   $('body').append('<form id="enroll2fa" method="post" action="' +
     G_SERVER + '/users/api/tfa/enable"><input type="hidden" id="enrollform-uid" name="uid"><input type="hidden" id="enrollform-password" name="password"></form>');
   let $helpform = $('#enroll2fa');
@@ -101,3 +146,137 @@ function enroll2fa() {
 }
 
 
+/**
+ * Checks to see if a username already exists in the DB
+ */
+function checkUsernameAvailability() {
+  let username = $('#lazyRegUsername').val();
+  let data = {username:username};
+  $.ajax({
+    method: 'POST',
+    url: '/users/api/check',
+    data: data,
+    success: displayUsernameAvailabilityMessage,
+    error: function(jqxhr) {
+      // silently fail . . .
+    }
+  });
+}
+
+/**
+ * Callback for checkUsernameAvailability
+ * @param {boolean} exists - status of the username availability
+ */
+function displayUsernameAvailabilityMessage(exists) {
+  if (exists) {
+    var msg = $('#template-user-exists').html();
+    $('#lazyRegUsernameError').html(msg)
+      .addClass('alert alert-danger col-lg-12');
+  } else {
+    $('#lazyRegUsernameError').html('')
+      .removeClass('alert alert-danger col-lg-12');
+  }
+  resizeFrame();
+}
+
+/**
+ * @param {Object:Event} e
+ */
+function validateLazyRegForm(e) {
+  e.preventDefault();
+
+  let valid = true;
+  let username = $('#lazyRegUsername').val().trim();
+  let email = $('#lazyRegEmail').val().trim();
+  if (!username.length || !username.match(/[a-z]/gi)) {
+    valid = false;
+  }
+  if (!email.length || !email.match(/[a-z]/gi)) {
+    valid = false;
+  }
+  if (valid) {
+    var data = $('#lazyRegForm').serialize();
+    $.ajax({
+      method: 'POST',
+      url: '/users/api/lazyregistration',
+      data: data,
+      success: function(response) {
+        if (response.error) {
+          $('#lazyRegStatus p')
+            .removeClass('alert-info')
+            .addClass('alert alert-danger')
+            .html(response.error.message);
+          resizeFrame();
+        } else {
+          var html = $('#template-lazy-reg-success').html();
+          $('#lazyRegStatus p').removeClass('alert-info').addClass('alert alert-success').html(html);
+          $('#resendEmail').click(function(el) {
+            el.preventDefault();
+            resendEmail(response.email, response.hash);
+          });
+          resizeFrame();
+         }
+      },
+      error: lazyRegError
+    });
+  }
+}
+
+/**
+ * Resend the confirmation email to the email with the hash provided from the first call
+ * @param {string} email - the user's email address
+ * @param {string} hash - the hash link for the account
+ */
+function resendEmail(email, hash) {
+  let url = '/users/api/lazyregistration-resend';
+  let data = { email:email, hash:hash };
+  $.ajax({
+    method: 'POST',
+    url: url,
+    data: data,
+    success: function(response) {
+      var error = response.error === 'true' ? true : false;
+      if (!error) {
+        var html = $('#lazyRegStatus p').html();
+        html += '<br>Another account confirmation email has been sent to the provided email address.';
+        $('#lazyRegStatus p').html(html);
+        $('#resendEmail').click(function(e) {
+          e.preventDefault();
+          resendEmail(email, hash);
+        });
+      }
+      // Don't handle error, email error should have come through the first time
+    },
+    error: function(jqxhr) {
+      // Silently fail . . .
+    }
+  });
+}
+
+/**
+ * Updates the DOM to reflect a failure to register using lazy reg
+ */
+function lazyRegError(message) {
+  $('#lazyRegStatus')
+    .html($('#template-lazy-reg-fail').html())
+    .find('p')
+    .removeClass('alert-info')
+    .addClass('alert alert-danger');
+}
+
+/**
+ * resized the From in the top window. Needs to run on same servername  */
+function resizeFrame(scrollToTop){
+      //resize drupal iframe
+    try {
+      var newHeight = (document.body.scrollHeight + 200).toString();
+      window.parent.document.getElementById('usermanagementframe').style.height = newHeight + 'px';
+      document.body.style.background = 'none';
+      top.history.replaceState({}, '', window.location.href);
+      if (scrollToTop) {
+        top.jQuery('html, body').animate({ scrollTop: 0 }, 300);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+}
---
+++ update.sh
@@ -0,0 +1,1 @@
+cp -r /var/www/drupal/cas-overlay-template/src/main/resources/* /var/lib/tomcat8/webapps/cas/WEB-INF/classes
